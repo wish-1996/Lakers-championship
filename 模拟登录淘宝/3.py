ffa8e0194732f02e2be391f67691ebcc5e4a56fd
wish-1996 <68992750+wish-1996@users.noreply.github.com>
2021-03-23 18:09:45 +0800

    Update 3.py

--- 模拟登录淘宝/3.py
+++ 模拟登录淘宝/3.py
@@ -1,8 +1,8 @@
 from login import Login
 
 
-username =  '357156851@qq.com'# 账号
-password = 'ypw985211'       # 密码
+username =  ''# 账号
+password = ''       # 密码
 # 初始化Login类
 login = Login(username, password)
 # 使用淘宝账号或手机号登录
